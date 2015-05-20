7df22f6c6ade1cd9c2691fb1cc3a55d02d22574d
Boris Ostojic <sergejspam@gmail.com>
2015-05-20 23:45:55 +0200

    Name variable imageName

--- set-backgrount-img.sh
+++ set-backgrount-img.sh
@@ -1,11 +1,11 @@
-now=$(date +'%Y%m%d%H%M').jpg
+imageName=$(date +'%Y%m%d%H%M').jpg
 # cur=$(pwd)
 cur=$(echo /home/boris/scripts/back-image)
 echo $cur
-wget http://www.kronplatz.com/_webcam/30/webcam3.jpg -O $cur/$now
+wget http://www.kronplatz.com/_webcam/30/webcam3.jpg -O $cur/$imageName
 
 # export DBUS_SESSION_BUS_ADDRESS environment variable
 PID=$(pgrep gnome-session)
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
 
-gsettings set org.gnome.desktop.background picture-uri file://$cur/$now
+gsettings set org.gnome.desktop.background picture-uri file://$cur/$imageName
