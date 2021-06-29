064c4960a5e0ce6864283804612d18fd3317e87b
dugokontov <dugokontov@gmail.com>
2021-06-29 12:59:00 +0200

    Enable load database even when started outside of root folder

--- raspberry-pi-server/backend/helper/db.js
+++ raspberry-pi-server/backend/helper/db.js
@@ -1,11 +1,13 @@
 const sqlite = require('sqlite');
 const sqlite3 = require('sqlite3');
+const path = require('path');
 
 sqlite3.verbose();
 
 async function getDb() {
+    const pathToDb = path.resolve(__dirname + '/../master.db');
     return await sqlite.open({
-        filename: './master.db',
+        filename: pathToDb,
         driver: sqlite3.cached.Database,
     });
 }
